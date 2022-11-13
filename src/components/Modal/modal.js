4b6db24c5f2b3c21d6c56dbee57dd0892341a04f
Samuel <samueladeyemi2006@gmail.com>
2022-11-13 17:29:46 +0100

    feat:  added modal component

--- src/components/Modal/modal.js
+++ src/components/Modal/modal.js
@@ -8,37 +8,42 @@ function Modal() {
      
       modalContainer.style.display ='none'
       document.body.style.background = "#fff";
-      document.body.style.overflowY = "scroll";
+      document.body.style.overflowY = "auto";
    } 
   return (
-    <div className='modal hidden'>
-        <div className='absolute left-[30%] top-[50%] bg-white shadow-md rounded-lg w-[28rem] h-[18rem] z-10'>
-          <div className='flex items-center justify-between p-5'>
-             <h2 className='text-lg'>Connect Wallet</h2>
-             <button onClick={handleCloseModal}>
-              <img src={Times} alt='close-icon' className='h-5'/>
-             </button>
+    <div className="fixed z-10  top-0 left-0 w-full h-full modal hidden">
+      <div className='flex items-center h-screen justify-center'>
+        <div className="bg-white rounded-lg w-[28rem] h-[18rem] z-10">
+          <div className="flex items-center justify-between p-5">
+            <h2 className="text-lg">Connect Wallet</h2>
+            <button onClick={handleCloseModal}>
+              <img src={Times} alt="close-icon" className="h-5" />
+            </button>
           </div>
-          <hr className='w-full' />
-          <div className='space-y-5 p-5'>
+          <hr className="w-full" />
+          <div className="space-y-5 p-5">
             <h4>Choose your preferred wallet:</h4>
-            {modalItems.map((items) =>{
-              return(   
-                <div key={items.id} className='p-1 rounded-lg flex items-center justify-between border-[1px]'>
-                  <div className='flex items-center space-x-2'>
-                    <img src={items.icon} alt={items.imgAlt} className='w-10'/>
+            {modalItems.map((items) => {
+              return (
+                <div
+                  key={items.id}
+                  className="p-1 rounded-lg flex items-center justify-between border-[1px]"
+                >
+                  <div className="flex items-center space-x-2">
+                    <img src={items.icon} alt={items.imgAlt} className="w-10" />
                     <h2>{items.text}</h2>
                   </div>
                   <div>
-                    <img src={items.chevron} alt='right-chevron' />
+                    <img src={items.chevron} alt="right-chevron" />
                   </div>
-               </div>
-              )
+                </div>
+              );
             })}
-         </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Modal
